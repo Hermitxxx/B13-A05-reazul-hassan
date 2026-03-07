8e303fc68d65ad8f7fdfce6d3bbd0e1cf1299e26
Hermitxxx <cvenom477@gmail.com>
2026-03-07 12:21:04 +0600

    added core functionality and responsiveness for mobile and medium devices

--- script/script.js
+++ script/script.js
@@ -14,7 +14,7 @@ async function loadAllIssues() {
 loadAllIssues()
 
 // get all section count
-function getCount(){
+function getCount() {
     let allIssueCount = document.getElementById("issue-count")
     // get all cards numbers
     const allCardsNumbers = document.getElementById("card-container").childElementCount;
@@ -58,6 +58,141 @@ function displayALlIssues(allIssues) {
         `
         cardContainer.appendChild(issueCard)
     }
+}
+
+// load all cards of open-section
+async function loadOpenData() {
+    const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
+    try {
+        const res = await fetch(url)
+        const json = await res.json();
+        displayOpenIssues(json.data)
+        getCount()
+    } catch (error) {
+        console.log("Something went wrong")
+    }
+}
+
+// display open data
+function displayOpenIssues(alldata) {
+    const openIssue = alldata.filter(item => item.status === "open");
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = ""
+
+    for (let issue of openIssue) {
+        const issueCard = document.createElement("div");
+        issueCard.innerHTML = `
+            <div id="${issue.id}" class="issue-card h-[28rem] w-auto border-t-4 ${updateCard(issue.status)} p-3 space-y-5 rounded-md shadow-md">
+                <div class="severity flex items-center justify-between">
+                    <div class="flex items-center justify-center w-9 h-9 rounded-full ${updateStatusCirlce(issue.status)}">
+                        ${statusCircleIcon(issue.status)}
+                    </div>
+                    <span class="badge px-7 py-3 rounded-full ${getPriorityClass(issue.priority)}">${issue.priority.toUpperCase()}</span>
+                </div>
+
+                <div id="issue-details" class="issue-details border-b border-b-gray-500/40 space-y-5 py-5">
+                
+                    <h2 class="font-semibold">${issue.title}</h2>
+                    <p>${issue.description}</p>
+                    <div class="issue-help flex items-center gap-2">
+                        ${updateBadgeLables(issue.labels)}
+                    </div>
+
+                </div>
+
+                <div class="issue-author">
+                    <p id="author-name" class="opacity-60">#${issue.id} by ${issue.author}</p>
+                    <p id="issue-date" class="opacity-60">${issue.createdAt}</p>
+                </div>
+            </div>
+        `
+        cardContainer.appendChild(issueCard)
+    }
+}
+
+// load closed issues
+async function loadClosedData() {
+    const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
+    try {
+        const res = await fetch(url)
+        const json = await res.json();
+        displayClosedIssues(json.data)
+        getCount()
+    } catch (error) {
+        console.log("Something went wrong")
+    }
+}
+
+// display closed issues
+function displayClosedIssues(alldata) {
+    const closedIssue = alldata.filter(item => item.status === "closed");
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = ""
+
+    for (let issue of closedIssue) {
+        const issueCard = document.createElement("div");
+        issueCard.innerHTML = `
+            <div id="${issue.id}" class="issue-card h-[28rem] w-auto border-t-4 ${updateCard(issue.status)} p-3 space-y-5 rounded-md shadow-md">
+                <div class="severity flex items-center justify-between">
+                    <div class="flex items-center justify-center w-9 h-9 rounded-full ${updateStatusCirlce(issue.status)}">
+                        ${statusCircleIcon(issue.status)}
+                    </div>
+                    <span class="badge px-7 py-3 rounded-full ${getPriorityClass(issue.priority)}">${issue.priority.toUpperCase()}</span>
+                </div>
+
+                <div id="issue-details" class="issue-details border-b border-b-gray-500/40 space-y-5 py-5">
+                
+                    <h2 class="font-semibold">${issue.title}</h2>
+                    <p>${issue.description}</p>
+                    <div class="issue-help flex items-center gap-2">
+                        ${updateBadgeLables(issue.labels)}
+                    </div>
+
+                </div>
+
+                <div class="issue-author">
+                    <p id="author-name" class="opacity-60">#${issue.id} by ${issue.author}</p>
+                    <p id="issue-date" class="opacity-60">${issue.createdAt}</p>
+                </div>
+            </div>
+        `
+        cardContainer.appendChild(issueCard)
+    }
+}
+
+// get triggered by the clicks on the toggles 
+const toggles = document.querySelector(".toggle-area");
+
+toggles.addEventListener("click", function (ev) {
+    const target = ev.target;
+
+    if (target.id === "all-btn") {
+        loadAllIssues()
+        removeActive(target)
+    }
+    else if (target.id === "open-btn") {
+        loadOpenData()
+        removeActive(target)
+    }
+    else if (target.id === "closed-btn") {
+        loadClosedData()
+        removeActive(target)
+    }
+})
+
+// toggle effect functionality
+function removeActive(target) {
+    const selected = target;
+    // get all btns 
+    const allToggles = document.querySelectorAll(".btn-toggle")
+    
+    // remove all active calsses (btn-primary)
+    allToggles.forEach(el => {
+        el.classList.remove("btn-primary")
+    });
+
+    // update the design for selected toggle
+    selected.classList.add("btn-primary")
 }
 
 // function for rendering cards on priority
@@ -110,20 +245,20 @@ function updateBadgeLables(labels) {
 }
 
 // update bade colors
-function updateBadgeColors(item){
-    if(item === "enhancement"){
+function updateBadgeColors(item) {
+    if (item === "enhancement") {
         return "border-green-300 bg-green-100 text-green-500"
     }
-    else if(item === "bug"){
+    else if (item === "bug") {
         return "border-red-300 bg-red-100 text-red-500"
     }
-    else if(item === "help wanted"){
+    else if (item === "help wanted") {
         return "border-purple-300 bg-purple-100 text-purple-500"
     }
-    else if(item === "documentation"){
+    else if (item === "documentation") {
         return "border-orange-300 bg-orange-100 text-orange-500"
     }
-    else{
+    else {
         return "border-gray-300 bg-gray-100 text-gray-500"
     }
 }
